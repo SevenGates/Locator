b7170edd7c1c0abb27274074ec6f523679aea279
SevenGates <alexander.hult14@gmail.com>
2016-03-25 05:51:19 +0100

    UI-updates

--- app/src/main/java/mah/sys/locator/SearchActivity.java
+++ app/src/main/java/mah/sys/locator/SearchActivity.java
@@ -24,8 +24,6 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         btnSearchProg = (Button)findViewById(R.id.buttonSearchProg);
         btnSearchRoom = (Button)findViewById(R.id.buttonSearchRoom);
@@ -57,27 +55,5 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
         String searchterm = textSearch.getText().toString();
         intent.putExtra("searchTerm",searchterm);
         return intent;
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_search, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
